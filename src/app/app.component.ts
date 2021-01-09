2ca9fe69a69b8df706a821007c55ce3a7c35264a
Ron Gerlach <wordsmith4u@hotmail.com>
2021-01-08 21:17:13 -0500

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bmdb-ng';
+  movies = ["Star Wars", "Sixteen Candles", "Fifth Element", "Better Off Dead", "Blazing Saddles", "Dr. Strangelove", "Monty Python & The Holy Grail", "Animal House", "This Is Spinal Tap", "Blues Brothers", "The Outlaw Josey Wales"];
 }
